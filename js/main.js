ff847f740203900097752ed5babb7dc69643a8c9
Tomas Unzue <tomas.unzue03@gmail.com>
2024-01-26 20:49:06 -0300

    primer comentario

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// perserverancia
+// tiempo
+// ganas de aprender
+// computadora
+// IntersectionObserverEntry
+
+
+//hola como estan, me llamo tomas unzue, soy software developer.
+
+//gracias por este trabajo de programador.
+//    Gracias por este trabajo de programador 
+
+// Gracias por este trabajo de programador.......
+// la programacion es el futuro \\\\\\\\\\
+
+
+
+//intentarlo todos los dias, no es facil pero se puede 
+
+
+// Estoy aprendiendo a programar hace casi dos anos, no fue un camino facil, pero por fin consegui mi primer trabajo como desarrollador web.
+
+
+
+let hora = prompt("Ingrese la hora actual")
